c3a8f33d38ed9b1fe6d115398648ac7e667c4c42
Winston Lowe <wel97459@gmail.com>
2018-06-02 01:39:28 -0700

    Change the AssemblyInfo.cs a little

--- VirtualKeyboard/Properties/AssemblyInfo.cs
+++ VirtualKeyboard/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtualKeyboard1")]
+[assembly: AssemblyTitle("VirtualKeyboard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtualKeyboard1")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("0ut$ider89")]
+[assembly: AssemblyProduct("VirtualKeyboard")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
